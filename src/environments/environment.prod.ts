1bf18bfa9c65f82b7598c52469f78ca3350dba14
Walter Lensinas <wlensinas@gmail.com>
2019-04-01 18:13:26 -0300

    Instalaci¢n de angular firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA9YcZb8MpX0SS0l9q7eh-JY7-Cy549E54",
+    authDomain: "platzinger-wl.firebaseapp.com",
+    databaseURL: "https://platzinger-wl.firebaseio.com",
+    projectId: "platzinger-wl",
+    storageBucket: "platzinger-wl.appspot.com",
+    messagingSenderId: "881578990887"
+  }
 };
