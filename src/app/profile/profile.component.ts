b9ea04065a0ce51ae6a4f2d41b666b24d6cc622b
Walter Lensinas <wlensinas@gmail.com>
2019-04-09 18:23:52 -0300

    Guardando las imagenes de perfil en firebase storage

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { User } from '../interfaces/user';
 import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-profile',
@@ -13,9 +14,10 @@ export class ProfileComponent implements OnInit {
   user: User;
   imageChangedEvent: any = '';
   croppedImage: any = '';
-
+  picture: any = '';
   constructor(private userService: UserService, 
-    private authenticationService: AuthenticationService) { 
+    private authenticationService: AuthenticationService,
+    private firebaseStorage: AngularFireStorage) { 
     this.authenticationService.getStatus().subscribe((status)=> {
       this.userService.getUserById(status.uid).valueChanges().subscribe((data: User)=> {
         this.user = data;
@@ -32,12 +34,33 @@ export class ProfileComponent implements OnInit {
   }
 
   saveSettings() {
-    this.userService.editUser(this.user).then( ()=> {
-      alert("Cambios guardados");
-    }).catch((err)=> {
-      alert("Hubo un error");
-      console.log(err);
-    })
+
+    if (this.croppedImage) {
+      const currentPictureId = Date.now();
+      const pictures = this.firebaseStorage.ref('pictures/' + currentPictureId + '.jpg').putString(this.croppedImage, 'data_url');
+
+      pictures.then( (result) => {
+        this.picture = this.firebaseStorage.ref('pictures/' + currentPictureId + '.jpg').getDownloadURL();
+        this.picture.subscribe((p)=> {
+         this.userService.setAvatar(p,this.user.uid).then(()=> {
+          alert('Avatar subido correctamente');
+         }).catch((err)=>{
+          alert('Hubo un error al subir la imagen');
+          console.log(err);
+          }) 
+        })
+      }).catch((err) => {
+        console.log(err);
+      })
+    } else {
+
+      this.userService.editUser(this.user).then( ()=> {
+        alert("Cambios guardados");
+      }).catch((err)=> {
+        alert("Hubo un error");
+        console.log(err);
+      })
+    }
   }
 
   fileChangeEvent(event: any): void {
