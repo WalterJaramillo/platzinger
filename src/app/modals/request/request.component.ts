558abe8f18a74a340552ec888a106bd8ed4f24d1
Walter Lensinas <wlensinas@gmail.com>
2019-05-11 14:02:42 -0300

    Reto: Agregando datos del amigo en el modal

--- src/app/modals/request/request.component.ts
+++ src/app/modals/request/request.component.ts
@@ -3,6 +3,7 @@ import { DialogService, DialogComponent } from 'ng2-bootstrap-modal';
 import { UserService } from 'src/app/services/user.service';
 
 import { RequestsService } from 'src/app/services/requests.service';
+import { User } from 'src/app/interfaces/user';
 
 
 export interface PromptModel {
@@ -20,6 +21,7 @@ export class RequestComponent extends DialogComponent<PromptModel, any> implemen
   scope: any;
   currentRequest: any;
   shouldAdd: string = 'yes';
+  friendUser: User;
 
   constructor(public dialogService: DialogService,
               private userService: UserService,
@@ -33,23 +35,24 @@ export class RequestComponent extends DialogComponent<PromptModel, any> implemen
       this.requestsService.setRequestStatus(this.currentRequest, 'accepted').then( (data)=> {
         console.log(data);
         this.userService.addFriend(this.scope.user.uid, this.currentRequest.sender).then( (res)=> {
-          alert('Solicitud aceptada con exito');
+          this.dialogService.removeAll();
+
         }).catch((err)=> {
           console.log(err);
-        })
+        });
       
       }).catch((err)=> {
         console.log(err);
-      })
+      });
     } else if(this.shouldAdd === 'no') {
       this.requestsService.setRequestStatus(this.currentRequest, 'rejected').then( (data)=> {
-        console.log(data);
+        this.dialogService.removeAll();
       }).catch((err)=> {
         console.log(err);
       })
     } else if(this.shouldAdd === 'later') {
       this.requestsService.setRequestStatus(this.currentRequest, 'decide_later').then( (data)=> {
-        console.log(data);
+        this.dialogService.removeAll();
       }).catch((err)=> {
         console.log(err);
       })
@@ -57,7 +60,12 @@ export class RequestComponent extends DialogComponent<PromptModel, any> implemen
   }
 
   ngOnInit() {
-    console.log('CurrentREQUEST');
-    console.log(this.currentRequest);
+    if (this.currentRequest.sender) {
+      this.userService.getUserById(this.currentRequest.sender).valueChanges().subscribe((data: User)=> {
+        this.friendUser = data;
+      }, (err)=> {
+        console.log(err);
+      });
+    }
   }
 }
